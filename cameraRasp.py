5dc3587de907def7332e4daf2384dcffd1629612
root <173194@ids.upchiapas.edu.mx>
2019-03-26 20:32:42 +0000

    Hola

--- cameraRasp.py
+++ cameraRasp.py
@@ -1,8 +1,8 @@
 from picamera import PiCamera
 from time import sleep
 import smtplib
-gmail_user = 'ayudacontustareas18@gmail.com'  
-gmail_password = 'jesucristo1'
+gmail_user = 'enriquegonz1323@gmail.com'
+gmail_password = 'aternosforeve23'
 import time
 from datetime import datetime
 from email.mime.image import MIMEImage
@@ -10,9 +10,9 @@ from email.mime.multipart import MIMEMultipart
 import RPi.GPIO as GPIO
 import time
 
-toaddr = 'enriquegonz1323@gmail.com'
-me = 'ayudacontustareas18@gmail.com'
-Subject='security alert'
+toaddr = 'ayudacontustareas18@gmail.com'
+me = 'enriquegonz1323@gmail.com'
+Subject='security aleeeeert'
 
 GPIO.setmode(GPIO.BCM)
 
@@ -23,18 +23,18 @@ P.start_preview()
 GPIO.setup(23, GPIO.IN)
 while True:
     if GPIO.input(23):
-        print("Motion...")
+        print("Movimiento...")
         #camera warm-up time
         time.sleep(1)
-        P.capture('movement.jpg')
+        P.capture('movement2.jpg')
         time.sleep(4)
         subject='Security allert!!'
         msg = MIMEMultipart()
         msg['Subject'] = subject
         msg['From'] = me
         msg['To'] = toaddr
-        
-        fp= open('movement.jpg','rb')
+
+        fp= open('movement2.jpg','rb')
         img = MIMEImage(fp.read())
         fp.close()
         msg.attach(img)
